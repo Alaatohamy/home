77a7d075969f235023c8d5237d53fa4d4a33c422
alaa <alaatohamy4@gmail.com>
2017-08-29 18:07:41 +0200

    Solving ATM problem

---
+++ text.rb
@@ -0,0 +1,20 @@
+ #!/home/alaa/.rbenv/shims/ruby
+
+puts  "Enter the Withdraw,please: "
+withdraw = gets.to_i
+fifty_bills = withdraw / 50
+remain = withdraw % 50 
+ten_bills = remain /10
+puts %Q{Your withdrow of #{withdraw} EGP produces:
+    #{fifty_bills} x 50 bills
+    #{ten_bills} x10 EGP bills}
+
+puts  "Enter the Withdraw,please: "
+withdraw = gets.to_i
+fifty_bills = withdraw / 50
+remain = withdraw % 50 
+ten_bills = remain /10
+puts %Q{Your withdrow of #{withdraw} EGP produces:
+    #{fifty_bills} x 50 bills
+    #{ten_bills} x10 EGP bills}
+
